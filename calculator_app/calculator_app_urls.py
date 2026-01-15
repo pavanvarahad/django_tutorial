8b1659a3095860c67bd670643d29ed5975676f06
Pavan Varahad <pavanvarahad@gmail.com>
2026-01-15 18:25:47 +0530

    JINJA

--- calculator_app/calculator_app_urls.py
+++ calculator_app/calculator_app_urls.py
@@ -2,6 +2,6 @@ from .views import calculator_view, jinja_tutorial
 from django.urls import path
 
 urlpatterns = [
-    path("", calculator_view, name="calculator_home"),
+    path("calculator", calculator_view, name="calculator_home"),
     path("jinja/", jinja_tutorial, name="jinja_tutorial"),
 ]
--- calculator_project/urls.py
+++ calculator_project/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("calculator_app.calculator_app_urls")),
+    path("", include("jinja_tutorial.jinja_urls")),
 ]
