4c329c51b45babf87247e4db5e338837fd5af678
Pavan Varahad <pavanvarahad@gmail.com>
2026-01-24 20:51:41 +0530

    Second Todo App

--- todo_second/todo_second_urls.py
+++ todo_second/todo_second_urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home,add_task
+from .views import home,add_task,show_all_tasks,task_completed,delete_task
 
 urlpatterns=[
-    path("/",home,name="home_page"),
-    path("/add_new_todo",add_task,name="add_task")
+    path("",home,name="home_page"),
+    path("add_new_todo",add_task,name="add_task"),
+    path("list",show_all_tasks,name="show_task"),
+    path("task_completed/<int:task_sl_no>",task_completed,name="task_completed"),
+    path("delete_task/<int:task_sl_no>",delete_task,name="delete_task")
 ]
--- todo_second/views.py
+++ todo_second/views.py
@@ -1,26 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # tasks =[
-#     {"task_name":"Learn Django",
+#     {
+    # "task_name":"Learn Django",
 #      "completed" : True
 #     },
 # ]
-tasks = []
+all_tasks = []
 
 # Create your views here.
 def home(request):
     return render(request,"todo_second/index.html")
 
+def show_all_tasks(request):
+    return render(request,"todo_second/list.html",{"all_tasks":all_tasks})
+
 def add_task(request):
-    new_task = request.POST.get('task')
+    print(type(request))
+    new_task = request.POST.get("task")
     task_dictionary = {
         "task_name": new_task,
         "completed": False
     }
-    tasks.append(task_dictionary)
+    all_tasks.append(task_dictionary)
     print("-"*80)
-    print(tasks)
+    print(all_tasks)
     print("-"*80)
-    return HttpResponse(tasks)
+    return redirect("show_task")
+
+def task_completed(request,task_sl_no):
+    all_tasks[task_sl_no-1]["completed"] = True
+    return redirect("show_task")
+
+def delete_task(request,task_sl_no):
+    all_tasks.pop(task_sl_no-1)
+    return redirect("show_task")
     
