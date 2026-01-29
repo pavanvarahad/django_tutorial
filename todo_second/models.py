67244de8a1e0dea4183840f90d7298a44c164df4
Pavan Varahad <pavanvarahad@gmail.com>
2026-01-29 19:32:58 +0530

    update

--- todo_second/models.py
+++ todo_second/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    # id = models.BigIntegerField(primary_key=True,auto_created=True)
+    id = models.BigAutoField(primary_key=True)
+    task_name = models.CharField(max_length=300)
+    is_completed = models.BooleanField()
