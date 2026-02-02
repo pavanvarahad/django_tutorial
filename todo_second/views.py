cb540c76933391964f61f763aeaf7116ee6b9b84
Pavan Varahad <pavanvarahad@gmail.com>
2026-02-02 19:38:06 +0530

    database operations

--- todo_second/views.py
+++ todo_second/views.py
@@ -1,40 +1,28 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Todo
-# tasks =[
-#     {
-    # "task_name":"Learn Django",
-#      "completed" : True
-#     },
-# ]
-all_tasks = []
 
 # Create your views here.
 def home(request):
     return render(request,"todo_second/index.html")
 
 def show_all_tasks(request):
-    return render(request,"todo_second/list.html",{"all_tasks":all_tasks})
+    all_db_rows = Todo.objects.all().values()
+    return render(request,"todo_second/list.html",{"all_tasks":all_db_rows})
 
 def add_task(request):
-    print(type(request))
     new_task = request.POST.get("task")
-    task_dictionary = {
-        "task_name": new_task,
-        "completed": False
-    }
-    Todo.objects.create(id=2,task_name=new_task,is_completed=False)
-    all_tasks.append(task_dictionary)
-    print("-"*80)
-    print(all_tasks)
-    print("-"*80)
+    Todo.objects.create(task_name=new_task,is_completed=True)
     return redirect("show_task")
 
 def task_completed(request,task_sl_no):
-    all_tasks[task_sl_no-1]["completed"] = True
+    selected_task = Todo.objects.get(id=task_sl_no)
+    selected_task.is_completed = True
+    selected_task.save()
     return redirect("show_task")
 
 def delete_task(request,task_sl_no):
-    all_tasks.pop(task_sl_no-1)
+    selected_task_to_delete = Todo.objects.get(id=task_sl_no)
+    selected_task_to_delete.delete()
     return redirect("show_task")
     
