39f5069d44d81e9c89cd8675987a2df44385c236
pavan <pavanvarahad.v@dxc.com>
2026-04-16 17:06:36 +0530

    Cleaned for better folder structure

--- sample_project/todo/todo_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home_view,add_new_task,delete,mark_completed
-
-urlpatterns = [
-    path('', home_view, name='home'),
-    path('add_task', add_new_task, name='add_task'),
-    path('delete_task',delete, name="delete"),
-    path('completed',mark_completed, name="completed")
-]
